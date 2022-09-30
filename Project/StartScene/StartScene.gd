cb60fd5b1957cdd1479a2c79c16a2d7e9a327417
Lyle Dotson <ld77668952@gmail.com>
2022-09-30 19:29:42 -0400

    add StartScene.ts

---
+++ Project/StartScene/StartScene.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+onready var _startGameObject = $Button
+var startGame :bool= false
+
+func _on_Button_pressed():
+	if _startGameObject.pressed == true:
+		startGame = true
+
+func _startGame():
+	if startGame == true:
+		get_tree().change_scene("res://EndScene/EndScene.tscn")
+		print(" Starting Game ...")
+
+func _process(_delta:float)-> void:
+	_startGame()
+	
